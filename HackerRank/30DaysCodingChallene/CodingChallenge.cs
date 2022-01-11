c3b22823e601c3eb3237ea4d9b905807a99fba85
UsmanAdebanjo <oluwatobilobalayeuthman@gmail.com>
2022-01-11 20:00:02 +0100

    Add project files.

---
+++ HackerRank/30DaysCodingChallene/CodingChallenge.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank._30DaysCodingChallene
+{
+    class CodingChallenge
+    {
+        public void Day1()
+        {
+
+
+        }
+
+        
+    }
+    //Day 4
+    public  class Person
+    {
+        public int age;
+        public Person(int initialAge)
+        {
+            // Add some more code to run some checks on initialAge
+            if (initialAge < 0)
+            {
+                Console.WriteLine("Age is not valid, setting age to 0.");
+                age = 0;
+            }
+            else
+            {
+                age = initialAge;
+            }
+        }
+            public void amIOld()
+            {
+            // Do some computations in here and print out the correct statement to the console 
+                if (age < 13)
+                {
+                    Console.WriteLine("You are young.");
+                }
+                else if (age >= 13 && age < 18)
+                {
+                    Console.WriteLine("You are a teenager.");
+                }
+                else
+                {
+                    Console.WriteLine("You are old.");
+                }
+            }
+
+            public  void yearPasses()
+            {
+                age++;
+            }
+        
+    }
+
+    public class Day9
+    {
+        public int Age {
+            get
+            {
+                return Age;
+            }
+
+            set
+            {
+                if (Age < 0)
+                {
+                    Age = 0;
+                }
+                else
+                {
+                    Age = value;
+                }
+            } 
+        }
+        public static int factorial(int n)
+        {
+            if (n > 1)
+            {
+                return n * factorial(n - 1);
+            }
+
+            return n;
+
+        }
+    }
+}
---
+++ HackerRank/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank._30DaysCodingChallene
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Day 4
+            int T = int.Parse(Console.In.ReadLine());
+            for (int i = 0; i < T; i++)
+            {
+                int age = int.Parse(Console.In.ReadLine());
+                Person p = new Person(age);
+                p.amIOld();
+                for (int j = 0; j < 3; j++)
+                {
+                    p.yearPasses();
+                }
+                p.amIOld();
+                Console.WriteLine();
+            }
+
+            /*End of Day 4*/
+
+
+
+            //Day 9
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            int result = Day9.factorial(n);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+
+            //End of Day 9
+
+        }
+    }
+
+
+}
